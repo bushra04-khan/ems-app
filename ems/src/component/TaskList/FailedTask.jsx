e7b7e143f3c68e7179ec8ea901dd90381a42e551
bushra04-khan <bushra.23b0151015@abes.ac.in>
2026-07-08 22:19:00 +0530

    Modified TaskList

---
+++ ems/src/component/TaskList/FailedTask.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const FailedTask = () => {
+    return (
+          <div className=' flex-shrink-0 h-full w-[300px] p-5 bg-blue-500 rounded-xl'>
+            <div className='flex justify-between items-center'>
+                <h3 className='bg-red-600 text-sm px-3 py-1 rounded'>High</h3>
+                <h4 className='text-sm'>04 July 2026</h4>
+
+            </div>
+            <h2 className='mt-5 text-2xl font-semibold'>Watch a React tutorial </h2>
+            <p className='text-sm mt-2'>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. In fuga magnam provident nesciunt vero magni.
+            </p>
+            <div className='mt-2'>
+                <button className='w-full'>Failed</button>
+
+            </div>
+        </div>
+    )
+}
+
+export default FailedTask;
--- ems/src/component/other/header.jsx
+++ ems/src/component/other/header.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
 const Header = ({data}) => {
-  console.log(data)
+ 
   return (
     <div className='flex items-center justify-between w-full'>
-      <h1 className='text-2xl font-medium text-white'>Hello <br /> <span className='text-3xl font-semibold'>Bushra 👋</span> </h1>
+      <h1 className='text-2xl font-medium text-white'>Hello <br /> <span className='text-3xl font-semibold'>{data.firstName} 👋</span> </h1>
       <button className='bg-red-600 text-lg font-medium text-white px-5 py-3 rounded-small'>Log Out</button>
     </div>
   )
